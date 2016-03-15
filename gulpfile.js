45e9f7e1bd35425424bf542403cb73feb4efa564
Jonas Büchel <jonas.buechel@gmail.com>
2016-03-15 20:11:40 +0100

    added gulp-task 'concatScripts', which allows to concatenate all my javaScript files into one app.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,22 @@
 
 /*jslint node: true*/
 
-var gulp = require('gulp');
+var gulp    = require('gulp'),
+    concat  = require('gulp-concat');
 
 //Testtask with name 'hello'
 gulp.task('hello', function(){
     console.log('I just say hello in the console!');
+});
+
+//Testtask with name 'hello'
+gulp.task('concatScripts', function(){
+    gulp.src([
+        'js/jquery.js', 
+        'js/sticky/jquery.sticky.js', 
+        'js/main.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('js'));
 });
 
 //set default task
