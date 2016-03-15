36143ddda62c430ad5d0ede924ed5fd25aac70e0
Jonas Büchel <jonas.buechel@gmail.com>
2016-03-15 20:31:19 +0100

    added a task 'minifiyScripts' to minimize the app.js and write it to app.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,30 @@
 /*jslint node: true*/
 
 var gulp    = require('gulp'),
-    concat  = require('gulp-concat');
+    concat  = require('gulp-concat'),
+    uglify  = require('gulp-uglify'),
+    rename  = require('gulp-rename');
 
 //Testtask with name 'hello'
 gulp.task('hello', function(){
     console.log('I just say hello in the console!');
 });
 
-//Testtask with name 'hello'
+//concatScripts: allows to bundle js files
 gulp.task('concatScripts', function(){
     gulp.src([
         'js/jquery.js', 
         'js/sticky/jquery.sticky.js', 
         'js/main.js'])
     .pipe(concat('app.js'))
+    .pipe(gulp.dest('js'));
+});
+
+//minifyScripts: allows to minify js files
+gulp.task('minifyScripts', function(){
+    gulp.src('js/app.js')
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
     .pipe(gulp.dest('js'));
 });
 
